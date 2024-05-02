78f73396010abdb2749008c42cc3c6de99a0c7be
Eliton Camargo <camargoliveira@gmail.com>
2024-05-02 18:47:10 -0300

    removendo node_modules

--- controllers/nutriente.js
+++ controllers/nutriente.js
@@ -3,17 +3,12 @@ import * as Nutriente from '../models/Nutriente.js';
 export const consultar = async (req, res)=>{
     try {
         let nome = req.query.nome;
+        nome = nome?nome:'';
         let result;
-        if (nome) {
-            result = await Nutriente.consultar(nome);
-        } else {
-            result = await Nutriente.consultar();
-        }
-
+        result = await Nutriente.consultar(nome);
         if (result.length > 0) {
             res.status(200).json(result);
         } else {
-            // Se nenhum resultado for encontrado, retornar 404
             res.status(404).json({ erro: 'Nenhum recurso encontrado' });
         }
     } catch (error) {
@@ -55,15 +50,10 @@ export const deletar = async (req, res)=>{
 
 export const cadastrada = async (req, res)=>{
     try {
-        const { nome, formula } = req.body; // Os dados enviados tenham campos 'nome' e 'formula'
-
-        // Chamar a função cadastrar com os dados recebidos
+        const { nome, formula } = req.body; 
         const novoNutriente = await Nutriente.cadastrar(nome, formula);
-
-        // Enviar uma resposta com os dados da novo Nutriente cadastrado
         res.status(201).json(novoNutriente);
     } catch (error) {
-        // Lidar com erros, se houver algum
         console.error('Erro ao cadastrar Nutriente:', error);
         res.status(500).json({ erro: 'Erro interno do servidor' });
     }
--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get('/',(req,res)=>{
 
 app.use('/', nutriente);
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Rota não encontrada' });
+});
 
 const PORT = 8080; 
 app.listen(PORT,()=>{
