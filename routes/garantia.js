3b2d57d0a19ae583eff392254f94eac0aabc9b30
Eliton Camargo <camargoliveira@gmail.com>
2024-05-08 21:05:29 -0300

    Criando funcionalidades para as funções de garantia

--- routes/garantia.js
+++ routes/garantia.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/garantia/nutriente/:nutrienteId', garantia.consultarPorNutriente);
 router.get('/garantia/materia_prima/:materia_primaId', garantia.consultarPorMateria_prima);
 router.post('/garantia', garantia.cadastrar);
-router.put('/garantia', garantia.atualizar);
+router.put('/garantia/:id', garantia.atualizar);
 router.delete('/garantia/:id', garantia.deletar);
 
 export default router;
