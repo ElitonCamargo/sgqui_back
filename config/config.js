2c4129a2fdce8766985ed74aa680a1a6cdb2a89b
Eliton Camargo <camargoliveira@gmail.com>
2024-09-04 23:13:01 -0300

    Conexão

--- config/config.js
+++ config/config.js
@@ -11,10 +11,10 @@ const developmentConfig = {
 const productionConfig = {
     host: process.env.DB_HOST || '100.26.59.163',
     port: parseInt(process.env.DB_PORT || '3306', 10),
-    name: process.env.BD_NAME || 'drawbe66_sgqui',
+    name: process.env.BD_NAME || 'api_sg_qui_dev',
     dialect: process.env.BD_DIALECT || 'mysql',
-    user: process.env.BD_USER || 'drawbe66_sgqui',
-    password: process.env.BD_PASS || 'Sg_Qui123'
+    user: process.env.BD_USER || 'admin_api',
+    password: process.env.BD_PASS || '%gt&j7&gfE0u1895'
 };
 
 export const db = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig;
